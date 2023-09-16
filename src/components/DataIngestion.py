63884cae89757d2abdc71caaccf40f9f252a6ddb
Sangamesh <sangamesh21199@gmail.com>
2023-09-16 22:32:43 +0530

    Both Model Training and hyper parameter tuning

--- src/components/DataIngestion.py
+++ src/components/DataIngestion.py
@@ -6,6 +6,7 @@ from src.logger import logging
 #import CustomException 
 import pandas as pd
 from src.components.DataTransformation import DataTransformation,DataTransformationConfig
+from src.components.ModelTrainer import ModelTrainer,ModelTrainerConfig
 
 
 from sklearn.model_selection import train_test_split
@@ -57,4 +58,7 @@ if __name__ == "__main__":
     train_data,test_data = obj.initate_data_ingestion()  
 
     data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)     
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)   
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
